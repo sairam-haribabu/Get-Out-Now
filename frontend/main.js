27c25647ab57c28744330b5ed71b76dc59a42d57
Kaylee Treviño <kayleetrevino@dyn-160-39-167-45.dyn.columbia.edu>
2022-04-24 19:50:47 -0400

    Home page gets names of results from search bar

--- frontend/main.js
+++ frontend/main.js
@@ -4,8 +4,24 @@ function searchEvents() {
     keyword = document.getElementById('keyword').value;
     console.log(keyword)
     sdk.searchGet({'query':keyword}, {}, {}).then((response) => {
-        console.log(response);
+        response = response['data']['body']
         document.getElementById('keyword').value = "";
+        const eventsDiv = document.createElement("div");
+
+        for (let key in response) {
+            eventsDiv.appendChild(document.createTextNode(key));
+            eventsDiv.appendChild(document.createElement("br"))
+
+            console.log(response[key])
+            for (let i in response[key]) {
+                let event = response[key][i]["Items"][0]['data']
+
+
+                eventsDiv.appendChild(document.createTextNode(event['name']));
+                eventsDiv.appendChild(document.createElement("br"))
+            }
+        }
+        document.getElementById('result-block').appendChild(eventsDiv)
     })
     .catch((error) => {
         console.log('an error occurred', error);
