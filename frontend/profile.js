a50224ed10f2fd3854444a6a28c8baaceb2326f1
Aishwarya Sivakumar <aishwaryasivakumar@dyn-209-2-210-38.dyn.columbia.edu>
2022-04-26 20:46:00 -0400

    Profile load using jquery

--- frontend/profile.js
+++ frontend/profile.js
@@ -1,22 +1,16 @@
 var sdk = apigClientFactory.newClient();
 
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
-});
-
-function loadProfile() {
     let username = localStorage.getItem('username');
     sdk.profileGet({'username':username}, {}, {}).then((response) => {
         response = response['data']['body']
         console.log(response, username, response['name'])
-        // document.getElementById('username').value = username;
-        // document.getElementById('name').value = response['name'];
-        // document.getElementById('bio').value = response['bio'];
-        $("#username").val(username)
-        $("#name").val(response['name'])
-        $("#bio").val(response['bio'])
+        $("#username").text(username)
+        $("#name").text(response['name'])
+        $("#bio").text(response['bio'])
     })
     .catch((error) => {
         console.log('an error occurred', error);
     });
-}
+});
