df6045d24e62a24d894a09a1919e523c5184cab7
Aishwarya Sivakumar <aishwaryasivakumar@dyn-209-2-210-38.dyn.columbia.edu>
2022-04-26 20:32:16 -0400

    Populate frontend of profile

--- frontend/profile.js
+++ frontend/profile.js
@@ -4,13 +4,17 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
-
-
 function loadProfile() {
     let username = localStorage.getItem('username');
     sdk.profileGet({'username':username}, {}, {}).then((response) => {
         response = response['data']['body']
-        console.log(response)
+        console.log(response, username, response['name'])
+        // document.getElementById('username').value = username;
+        // document.getElementById('name').value = response['name'];
+        // document.getElementById('bio').value = response['bio'];
+        $("#username").val(username)
+        $("#name").val(response['name'])
+        $("#bio").val(response['bio'])
     })
     .catch((error) => {
         console.log('an error occurred', error);
